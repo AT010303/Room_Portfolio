c4a06664c938776fb213edc4a3fce9d1565f7f50
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-03-07 00:01:41 +0530

    added loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import './style.css';
 
+import { Loader } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { EffectComposer } from '@react-three/postprocessing';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Experience from './Experience.jsx';
@@ -11,17 +13,22 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 const created = () => {};
 
 root.render(
-    <Canvas
-        camera={{
-            fov: 35,
-            near: 0.1,
-            far: 200,
-            position: [10, 5, -10]
-        }}
-        onCreated={created}
-    >
-        <EffectComposer enabled:true disableNormalPass:true>
-            <Experience />
-        </EffectComposer>
-    </Canvas>
+    <>
+        <Canvas
+            camera={{
+                fov: 35,
+                near: 0.1,
+                far: 200,
+                position: [10, 5, -10]
+            }}
+            onCreated={created}
+        >
+            <Suspense fallback:null>
+                <EffectComposer enabled:true disableNormalPass:true>
+                    <Experience />
+                </EffectComposer>
+            </Suspense>
+        </Canvas>
+        <Loader />
+    </>
 );
