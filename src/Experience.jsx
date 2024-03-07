174202e7175e87f5466d20d00bd0339229d79517
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-03-07 23:40:38 +0530

    removed pan

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { CameraControls  } from '@react-three/drei';
+import { CameraControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
 import RoomModel from './RoomModel/roomModel';
@@ -7,13 +7,13 @@ export default function Experience() {
     return (
         <>
             <Perf position="top-left" />
-            <CameraControls 
-                dollyToCursor={true} 
+            <CameraControls
+                dollyToCursor={true}
                 dollySpeed={0.2}
-                truckSpeed={0.2}
-                minDistance={3} 
-                maxDistance={25} 
-                smoothTime={2} 
+                truckSpeed={0}
+                minDistance={3}
+                maxDistance={25}
+                smoothTime={2}
                 maxAzimuthAngle={Math.PI}
                 minAzimuthAngle={Math.PI * 0.5}
                 minPolarAngle={Math.PI * 0.1}
@@ -21,9 +21,8 @@ export default function Experience() {
                 makeDefault
                 polarRotateSpeed={0.1}
                 azimuthRotateSpeed={0.1}
-                truck={false}
                 maxSpeed={20}
-                 />
+            />
             <RoomModel />
         </>
     );
