dd4aa8fe1646e278606a1f5801091f1283853337
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-05-23 22:02:23 +0530

    just prettier run

--- src/RoomModel/iframes/desktopiFrame.jsx
+++ src/RoomModel/iframes/desktopiFrame.jsx
@@ -3,28 +3,26 @@ import { Html } from '@react-three/drei';
 import { useCameraStore } from '../../helper/CameraStore';
 
 export default function DesktopiFrame() {
-
     const cameraState = useCameraStore((state) => state.cameraState);
     return (
         <group>
-            
-                <Html
-                    rotation-y={Math.PI}
-                    transform
-                    wrapperClass="htmlScreen"
-                    distanceFactor={0.52}
-                    occlude="blending"
-                    position={[2.125, 3.026, 3.69]}
-                    zIndexRange={cameraState === 'desktop' ? [100, 0] : [-1, 0]}
-                >
-                    <iframe
-                        width={1511}
-                        height={850}
-                        title="embed"
-                        src="https://portfolio-inner-theta.vercel.app/"
-                        frameBorder={0}
-                    />
-                </Html>
+            <Html
+                rotation-y={Math.PI}
+                transform
+                wrapperClass="htmlScreen"
+                distanceFactor={0.52}
+                occlude="blending"
+                position={[2.125, 3.026, 3.69]}
+                zIndexRange={cameraState === 'desktop' ? [100, 0] : [-1, 0]}
+            >
+                <iframe
+                    width={1511}
+                    height={850}
+                    title="embed"
+                    src="https://portfolio-inner-theta.vercel.app/"
+                    frameBorder={0}
+                />
+            </Html>
         </group>
     );
 }
